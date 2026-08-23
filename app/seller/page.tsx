857d41d89d3d87b05472dcb8063ef9b466f0f0ad
Vaibhav <vaibhavshivhare1709@gmail.com>
2026-08-23 18:20:04 +0530

    feat: implement EditEvent and NewEvent pages with event form integration

--- app/seller/page.tsx
+++ app/seller/page.tsx
@@ -1,19 +1,15 @@
-import React from 'react'
 import { auth} from "@clerk/nextjs/server";
 import SellerDashboard from '@/components/SellerDashboard';
 import { redirect } from 'next/navigation';
 
-async function SellerPage() {
+export default async function SellerPage() {
   const { userId } = await auth();
   if(!userId) redirect('/');
 
-
-
   return (
-    <div className="min-h-screen bg-gray-50">SELLERPAGE 
-        < SellerDashboard/>
+    <div className="min-h-screen bg-gray-50"> 
+        <SellerDashboard/>
     </div>
   )
 }
 
-export default SellerPage
